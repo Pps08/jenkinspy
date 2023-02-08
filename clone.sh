6a2e59b554a3458fd2c09cdd5ec09a763692642c
Pps08 <108321249+Pps08@users.noreply.github.com>
2023-02-08 17:13:05 +0530

    added escape slash

--- clone.sh
+++ clone.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 MY_PATH=${WORKSPACE}
-cd \\\livoffice04\\crm\\SkyCRM\\PriyaParthasarathy
+cd \\\\livoffice04\\crm\\SkyCRM\\PriyaParthasarathy
 git clone ssh://git@ssh.github.com:443/Pps08/jenkinspy.git
