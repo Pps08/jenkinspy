b6faa8604f0885f87ab5de1a03ab8b4b644a4d17
Pps08 <108321249+Pps08@users.noreply.github.com>
2023-02-08 16:44:24 +0530

    testing2

--- clone.sh
+++ clone.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 MY_PATH=${WORKSPACE}
 cd "${WORKSPACE}"
-git clone https://pps08:Pp!9176277347@github.com/sky-uk/dta-customer-tf 
+ssh -T -p 443 git@ssh.github.com
+git clone ssh://git@ssh.github.com:443/Pps08/jenkinspy.git
 cd "${WORKSPACE}/dta-customer-tf"
