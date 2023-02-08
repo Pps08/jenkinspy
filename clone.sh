f1ef71c4fd6883720a1200b40fde8c6ab78729b2
Pps08 <108321249+Pps08@users.noreply.github.com>
2023-02-08 16:45:55 +0530

    testing 3

--- clone.sh
+++ clone.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 MY_PATH=${WORKSPACE}
 cd "${WORKSPACE}"
-ssh -T -p 443 git@ssh.github.com
+ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
 git clone ssh://git@ssh.github.com:443/Pps08/jenkinspy.git
 cd "${WORKSPACE}/dta-customer-tf"
