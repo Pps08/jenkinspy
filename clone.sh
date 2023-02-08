88abdbb08c3944f1075319edc2571ef7c377a197
Pps08 <108321249+Pps08@users.noreply.github.com>
2023-02-08 16:38:58 +0530

    add git cred

--- clone.sh
+++ clone.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 MY_PATH=${WORKSPACE}
 cd "${WORKSPACE}"
-git clone git@github.com:Pps08/jenkinspy.git
+git clone https://pps08:Pp!9176277347@github.com/sky-uk/dta-customer-tf 
 cd "${WORKSPACE}/dta-customer-tf"
