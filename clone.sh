dbff9816b345ab6a7d77988138608bb5970527c8
Pps08 <108321249+Pps08@users.noreply.github.com>
2023-02-09 13:37:10 +0530

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -3,5 +3,4 @@ MY_PATH=${WORKSPACE}
 cd ${WORKSPACE}
 GIT_USER=PPS08
 GIT_TOKEN=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
-GIT_API="https://$GIT_USER:$GIT_TOKEN@api.github.com"
-git clone $GIT_API/orgs/Pps08/jenkinspy
+git clone https://02b772a70f3c23e41d2231adec6778ac392cebd4@github.com/Pps08/jenkinspy.git
