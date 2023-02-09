9ec2dac2c0affbd77b126e377a3368425da1251b
Pps08 <108321249+Pps08@users.noreply.github.com>
2023-02-09 11:50:35 +0530

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 MY_PATH=${WORKSPACE}
 cd ${WORKSPACE}
-git clone https://github.com/sky-uk/dta-customer-tf
+GIT_USER=SVC-DTO-GH-RO
+GIT_TOKEN=02b772a70f3c23e41d2231adec6778ac392cebd4
+GIT_API="https://$GIT_USER:$GIT_TOKEN@api.github.com"
+git clone $GIT_API/orgs/Pps08/jenkinspy
