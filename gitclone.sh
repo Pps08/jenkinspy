b47a73beeaff3da42d322709eb2fc9d1e2c17169
Pps08 <108321249+Pps08@users.noreply.github.com>
2023-02-10 18:24:36 +0530

    Update gitclone.sh

--- gitclone.sh
+++ gitclone.sh
@@ -5,7 +5,7 @@ git clone https://02b772a70f3c23e41d2231adec6778ac392cebd4@github.com/Pps08/jenk
 cd "${WORKSPACE}/jenkinspy"
 #"Create a temp branch from dev branch and add code to it"
 echo "Enter temp branch name to be created"
-git branch "$temp_branch" develop
+git checkout -b "$temp_branch"
 git push -u origin "$temp_branch"
 git checkout "$temp_branch"
 mkdir "${WORKSPACE}/jenkinspy/temp2"
@@ -20,7 +20,7 @@ git commit -m "Adding new folder"
 git push origin "$temp_branch"
 #"Create jira branch from latest dev  and add temp branch code into it"
 echo "Enter jira branch name to be created"
-git branch "$MY_BRANCH" develop
+git checkout -b "$MY_BRANCH"
 git push -u origin "$MY_BRANCH"
 git checkout "$MY_BRANCH"
 echo "Enter absolute path of the script to be merged into jira branch"
