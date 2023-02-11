3b5cf1c5d3012815f685e2b71e4ef57fe37fc532
Pps08 <108321249+Pps08@users.noreply.github.com>
2023-02-11 13:08:37 +0530

    Update gitclone.sh

--- gitclone.sh
+++ gitclone.sh
@@ -38,7 +38,7 @@ git checkout "$MY_BRANCH"
 git merge "$temp_branch" -m "Merge branches"
 #git push origin "$MY_BRANCH" 
 #git remote set-url origin https://02b772a70f3c23e41d2231adec6778ac392cebd4@github.com/Pps08/jenkinspy.git
-git remote set-url origin https://pps08:02b772a70f3c23e41d2231adec6778ac392cebd4@github.com/Pps08/jenkinspy.git
+git remote set-url origin https://SVC-DTO-GH-RO:02b772a70f3c23e41d2231adec6778ac392cebd4@github.com/Pps08/jenkinspy.git
 #https://pps08:Pp!9176277347
 git push -u origin "$temp_branch" 
 git push -u origin "$MY_BRANCH"
