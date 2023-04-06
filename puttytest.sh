90010493c5faca5b4d9c331b3321860915b5625f
Pps08 <108321249+Pps08@users.noreply.github.com>
2023-04-06 16:58:54 +0530

    Update puttytest.sh

--- puttytest.sh
+++ puttytest.sh
@@ -1,17 +1,16 @@
-echo "hi"
 username='priya_parthasarathy_sky_uk'
 hostname='10.53.88.219'
-PPK_FILE="C:\Users\pps08\.gcp_putty_ssh\id_rsa.ppk" 
+#PPK_FILE="C:\Users\pps08\.gcp_putty_ssh\id_rsa.ppk" 
 SSH_FILE="C:\Users\pps08\.gcp_putty_ssh\ssh.txt" 
 #puttygen_path="C:\Program Files\PuTTY\puttygen.exe"
 #"$puttygen_path" "$ppk_file" -O private-openssh -o "$SSH_FILE"
 #puttygen $PPK_FILE -O private-openssh -o $SSH_KEY_FILE
 command="/apps/release_manager/batch_application/scripts/scr/build_application.sh -rep dta-customer-tf -cfg BBR-398_tf-202303_01.json -replace -branch BBR-398"
 #putty.exe -ssh $username@$hostname -i $PPK_FILE "ls -la
-echo "inside script"
 #ssh -i $PPK_FILE $username@$hostname " rm -r /home/$username/archive/releases/* ;/apps/release_manager/batch_application/scripts/scr/build_application.sh -rep dta-customer-tf -cfg BBR-399_tf-202303_01.json -replace -branch BBR-399"
 #ssh -i $PPK_FILE $username@$hostname "pwd"
-ssh -i $SSH_FILE $username@$hostname "
+#echo  "$username"
+ssh -i "$SSH_FILE" "$username"@"$hostname" -o StrictHostKeyChecking=no "
 if [ -d ""/home/$username/archive/releases/*"" ]
 then
 	rm -r /home/$username/archive/releases/* ;$command
