c3ea5e2494d734d9b20368f22468bf15013efd1e
Pps08 <108321249+Pps08@users.noreply.github.com>
2023-04-07 16:40:56 +0530

    Update gitclone.sh

--- gitclone.sh
+++ gitclone.sh
@@ -59,4 +59,4 @@ PRbody=$(<"${WORKSPACE}\PRbody.txt")
 #"${WORKSPACE}"/gh auth login
 #"${WORKSPACE}"/gh auth login -h github.com --with-token < "${WORKSPACE}"/GH_Token.txt
 #gh auth login -h github.com --with-token 
-gh pr create --head "$MY_BRANCH" --title "$PRtitle" --body "$PRbody" --draft
+"${WORKSPACE}"/gh pr create --head "$MY_BRANCH" --title "$PRtitle" --body "$PRbody" --draft
