7d646b8a4baa93a3626671ddead10e2189440b71
Pps08 <108321249+Pps08@users.noreply.github.com>
2023-04-07 16:16:23 +0530

    Update gitclone.sh

--- gitclone.sh
+++ gitclone.sh
@@ -44,10 +44,11 @@ echo "pushing to remote"
 #Create Draft PR
 git checkout "$MY_BRANCH"
 echo "creating PR"
-echo "$(<"${WORKSPACE}\PRbody.txt")" 
+#echo "$(<"${WORKSPACE}\PRbody.txt")" 
 PRbody=$(<"${WORKSPACE}\PRbody.txt")
+echo $GH_TOKEN | GH_TOKEN= "${WORKSPACE}"/gh auth login --with-token
 #GH_TOKEN='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'
-"${WORKSPACE}"/gh auth login
+#"${WORKSPACE}"/gh auth login
 #"${WORKSPACE}"/gh auth login -h github.com --with-token < "${WORKSPACE}"/GH_Token.txt
 #gh auth login -h github.com --with-token 
 "${WORKSPACE}"/gh pr create --head "$MY_BRANCH" --title "$PRtitle" --body "$PRbody" --draft
