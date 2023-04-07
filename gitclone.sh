03097d712422107f9460a4a55866342ac73cca07
Pps08 <108321249+Pps08@users.noreply.github.com>
2023-04-07 16:33:40 +0530

    Update gitclone.sh

--- gitclone.sh
+++ gitclone.sh
@@ -46,7 +46,14 @@ git checkout "$MY_BRANCH"
 echo "creating PR"
 #echo "$(<"${WORKSPACE}\PRbody.txt")" 
 PRbody=$(<"${WORKSPACE}\PRbody.txt")
-curl -u pps08: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 https://api.github.com/user
+GIT_TOKEN=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
+GIT_API="https://api.github.com"
+ curl -s -L \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $GIT_TOKEN"\
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  $GIT_API
+#curl -u pps08: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 https://api.github.com/user
 #echo $GH_TOKEN | GH_TOKEN= "${WORKSPACE}"/gh auth login --with-token
 #GH_TOKEN='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'
 #"${WORKSPACE}"/gh auth login
