01eec0fc5f77627274c3664ae55a9398d68e2262
Pps08 <108321249+Pps08@users.noreply.github.com>
2023-04-10 10:49:09 +0530

    Update gitclone.sh

--- gitclone.sh
+++ gitclone.sh
@@ -69,5 +69,5 @@ PRbody=$(<"${WORKSPACE}\PRbody.txt")
 set +u
 echo "$GITHUB_TOKEN" > .githubtoken
 unset GITHUB_TOKEN
-gh auth login --with-token < .githubtoken
+#"${WORKSPACE}"\\gh auth login --with-token < .githubtoken
 rm .githubtoken
