c8aff24dc822d83272808e72b61b4e21c0949a5a
Pps08 <108321249+Pps08@users.noreply.github.com>
2023-04-11 10:17:52 +0530

    Create sendmail.pl

---
+++ sendmail.pl
@@ -0,0 +1,16 @@
+#!/bin/perl -w
+use MIME::Lite;
+$to = $recepient;
+$from = $recepient;
+$subject = 'QG mail';
+$message = $deploycommand;
+ 
+$msg = MIME::Lite->new(
+From => $from,
+To => $to,
+Subject => "$subject",
+Type => 'text/html',
+Data => $message);
+
+$msg->send('smtp','bridgeheads.bskyb.com',From => $from);
+
