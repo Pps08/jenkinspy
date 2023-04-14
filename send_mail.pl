64a8fbb22cdfbda6ca97a39d3ff5fb0d5c2fab76
Pps08 <108321249+Pps08@users.noreply.github.com>
2023-04-14 17:37:05 +0530

    Update send_mail.pl

--- send_mail.pl
+++ send_mail.pl
@@ -1,7 +1,7 @@
 use Mail::Sendmail;
  
-%mail = ( To      => "priya.parthasarathy@sky.uk",
-          From    => "priya.parthasarathy@sky.uk",
+%mail = ( To      => 'priya.parthasarathy@sky.uk',
+          From    => 'priya.parthasarathy@sky.uk,
           Message => "Deploycommand"
          );
  
