f2d65455c736d01730a083053ead55ba31ac4136
Pps08 <108321249+Pps08@users.noreply.github.com>
2023-04-14 11:41:17 +0530

    Create sendmail.py

---
+++ sendmail.py
@@ -0,0 +1,13 @@
+import sys
+import win32com.client as win32
+outlook=win32.Dispatch('outlook.application')
+mail=outlook.CreateItem(0)
+outlook=win32.Dispatch('outlook.application')
+#df2.reset_index()
+outlook=win32.Dispatch('outlook.application')
+mail=outlook.CreateItem(0)
+mail.To=sys.argv[1]
+mail.Subject="QG mail"
+mail.HTMLBody=sys.argv[2]
+mail.Send()
+print("Mail sent")
