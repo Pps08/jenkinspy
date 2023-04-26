3259c658636b3a8ec462cfc50a890c9391721480
Pps08 <108321249+Pps08@users.noreply.github.com>
2023-04-26 13:39:07 +0530

    Update gitclone.sh

--- gitclone.sh
+++ gitclone.sh
@@ -68,15 +68,15 @@ echo "creating PR"
 #echo "creating Pull Request"
 #"${WORKSPACE}"\\gh pr create --head "$latest_branch" --title "$PRtitle" --body "$PRbody" --draft
 git config --global credential.helper wincred
-set +u
-echo "$GITHUB_TOKEN"
-echo "$GITHUB_TOKEN" > .githubtoken
+#set +u
+#echo "$GITHUB_TOKEN"
+#echo "$GITHUB_TOKEN" > .githubtoken
 git push origin "$temp_branch" 
 git push origin "$latest_branch"
 "${WORKSPACE}"\\gh auth login --with-token < .githubtoken
 "${WORKSPACE}"\\gh pr create --head "$latest_branch" --title "$PRtitle" --body "$PRbody" --draft
-unset GITHUB_TOKEN
-rm .githubtoken
+#unset GITHUB_TOKEN
+#rm .githubtoken
 #/usr/bin/perl ${WORKSPACE}/sendmail.pl $recepient $deploycommand
 #${WORKSPACE}/sendmail.py  "$recepient" "$deploycommand"
 #/usr/bin/perl ${WORKSPACE}/send_mail.pl $recepient $deploycommand
